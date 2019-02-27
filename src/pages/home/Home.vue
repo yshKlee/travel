e5eac1074e2d0de33653cccbd1423e2ba21f98dd
yshKlee <timeisfault@qq.com>
2019-02-27 17:58:56 +0800

    'swiper'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <home-header />
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
-  components: { HomeHeader }
+  components: { HomeHeader, HomeSwiper }
 }
 </script>
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="wapper">
+    <swiper :options="swiperOption"
+            ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide v-for="(item) in swiperList"
+                    :key="item.id">
+        <img class="swiper-img"
+             :src="item.imgUrl"
+             alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"
+           slot="pagination"></div>
+      <!-- <div class="swiper-button-prev"
+         slot="button-prev"></div>
+    <div class="swiper-button-next"
+         slot="button-next"></div> -->
+      <!-- <div class="swiper-scrollbar"
+         slot="scrollbar"></div> -->
+    </swiper>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'https://imgs.qunarzz.com/vc/ad/7b/69/c28e29fe012cc64e4ec015b82d.jpg_92.jpg'
+      }, {
+        id: '002',
+        imgUrl: 'https://imgs.qunarzz.com/vc/47/23/20/cb678ea58b7b9e86be4f863e55.jpg_92.jpg'
+      }]
+    }
+  },
+  computed: {
+    swiper () {
+      return this.$refs.mySwiper.swiper
+    }
+  },
+  mounted () {
+    console.log('this is current swiper instance object', this.swiper)
+    // this.swiper.slideTo(3, 100, false)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wapper >>> .swiper-pagination-bullet-active
+  background #fff
+.wapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 57%
+  .swiper-img
+    width 100%
+</style>
