9c7ee9da8caf2896d31c7b89d2c1542c157cffe4
yshKlee <timeisfault@qq.com>
2019-03-04 11:22:05 +0800

    'home_re'

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <div tag="li"
+           class="item border-bottom"
+           v-for="item of list"
+           :key="item.id">
+        <img class="item-img"
+             :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      list: [{
+        id: 1,
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png',
+        title: 'qqq',
+        desc: 'www'
+      }, {
+        id: 2,
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png',
+        title: 'qqq',
+        desc: 'www'
+      }, {
+        id: 3,
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png',
+        title: 'qqq',
+        desc: 'www'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top 0.2rem
+  line-height 0.8rem
+  background #eee
+  text-indent 0.2rem
+.item
+  overflow hidden
+  display flex
+  height 1.9rem
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding 0.1rem
+  .item-info
+    flex 1
+    padding 0.1rem
+    min-width 0
+    .item-title
+      line-height 0.54rem
+      font-size 0.32rem
+      ellipsis()
+    .item-desc
+      line-height 0.4rem
+      color #ccc
+      ellipsis()
+    .item-button
+      line-height 0.44rem
+      margin-top 0.16rem
+      background #ff9300
+      padding 0 0.2rem
+      border-radius 0.06rem
+      color #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of list"
+          :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img"
+               :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      list: [
+        {
+          'id': '0003',
+          'imgUrl': 'http://img1.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+          'desc': '必游榜单',
+          'title': 'aaa'
+        }, {
+          'id': '0004',
+          'imgUrl': 'http://img1.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+          'desc': '必游榜单',
+          'title': 'aaa'
+        },
+        {
+          'id': '0005',
+          'imgUrl': 'http://img1.qunarzz.com/sight/source/1811/52/b6776b49d27025.jpg_r_640x214_35ae205c.jpg',
+          'desc': '必游榜单',
+          'title': 'aaa'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  line-height 0.8rem
+  background #eee
+  text-indent 0.2rem
+.item-img-wrapper
+  overflow hidden
+  height 0
+  padding-bottom 37.09%
+  .item-img
+    width 100%
+.item-info
+  padding 0.1rem
+  .item-title
+    line-height 0.54rem
+    font-size 0.32rem
+    ellipsis()
+  .item-desc
+    line-height 0.4rem
+    color #ccc
+    ellipsis()
+</style>
